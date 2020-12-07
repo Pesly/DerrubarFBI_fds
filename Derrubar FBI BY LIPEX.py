e157e9c187cc107cdd488fee790661c9f262b581
Pesly <gglipex@gmail.com>
2020-12-07 04:38:38 -0300

    Derrubar fbi e isso

---
+++ Derrubar FBI BY LIPEX.py
@@ -0,0 +1,84 @@
+print("""                                                                                                                                                                                                                                               """)
+
+
+
+print("""# #######  #####     ###### #
+# ##       ##   ##     ## #
+# ##       ##   ##     ## #
+# #####    ######      ## #
+# ##       ##   ##     ## #
+# ##       ##   ##     ## #
+# ##       #####     ###### #
+
+""")
+
+
+
+
+
+
+
+
+print('\033[1;37m > By LIPEX/PESLY......')
+
+
+
+print('\033[1;37m='*50)
+
+print('                              ')
+
+lx1 = str(input('\033[1;30m -> Digite 1 para derrubar o FBI: '))
+lx2 = input('\033[1;30m -> Deseja mesmo ir em frente? Se sim digite 2: ')
+
+print('                              ')
+
+print('\033[1;37m[+] Tudo pronto meu jovem, por questões de seguraça informe seu nome a seguir.....')
+
+print('                              ')
+
+lx3 = input('\033[1;37m -> Nome: ')
+
+print('                              ')
+
+print('\033[1;32mOk, Vamos lá!!!')
+lx4 = input('[+] Tecle ENTER...')
+
+print('                              ')
+
+print('\033[1;32m＝'*25)
+
+print('                              ')
+
+print('\033[2;32m[ + ] 𝐒𝐭𝐚𝐫𝐭𝐢𝐧𝐠 𝐡𝐚𝐜𝐤𝐢𝐧𝐠....')
+print('\033[2;32m[ + ] 𝐇𝐚𝐜𝐤𝐢𝐧𝐠 𝐅𝐁𝐈 𝟎%')
+print('\033[2;32m[ + ] 𝐇𝐚𝐜𝐤𝐢𝐧𝐠 𝐅𝐁𝐈 𝟐𝟎%')
+print('\033[2;32m[ + ] 𝐇𝐚𝐜𝐤𝐢𝐧𝐠 𝐅𝐁𝐈 𝟒𝟎%')
+print('\033[2;32m[ + ] 𝐇𝐚𝐜𝐤𝐢𝐧𝐠 𝐅𝐁𝐈 𝟔𝟎%')
+print('\033[2;32m[ + ] 𝐇𝐚𝐜𝐤𝐢𝐧𝐠 𝐅𝐁𝐈 𝟖𝟎%')
+print('\033[2;32m[ + ] 𝐇𝐚𝐜𝐤𝐢𝐧𝐠 𝐅𝐁𝐈 𝟏𝟎𝟎%')
+print('\033[2;34m[ + ] 𝐅𝐁𝐈 𝐇𝐚𝐜𝐤𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲\033[m')
+
+print('                              ')
+
+print('\033[1;32m＝'*25)
+
+print('                              ')
+
+print('\033[1;37m [-] Parabéns {}{}{}\033[1;37m, Você em ótimo Hacker.... KKKKKKKKKKKKKKKKKKKKK VSFD!!'.format('\033[1;34m', lx3,'\033[m'))
+
+print('                              ')
+
+print('\033[1;32m＝'*25)
+
+print('                              ')
+
+lx6 = input('\033[1;34m -> Digite 0 para sair do script:\033[m ')
+print('\033[1;34m𝐒𝐚𝐢𝐧𝐝𝐨....')
+
+print('                              ')
+
+print('\033[1;32m＝'*25)
+
+print('                              ')
+
+
